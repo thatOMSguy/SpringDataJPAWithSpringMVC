4e66586637017f075562f23cfa55a8d6346333b0
Ravi Kumar <79511972+raviKumar-bsgi@users.noreply.github.com>
2023-03-30 17:30:30 +0530

    Added Spring JPA repository for Beer and Customer

---
+++ src/main/java/com/springrestmvcproject/spring6restmvc/repositories/BeerRepository.java
@@ -0,0 +1,6 @@
+package com.springrestmvcproject.spring6restmvc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeerRepository extends JpaRepository {
+}
